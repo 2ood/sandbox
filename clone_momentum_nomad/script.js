1cd532236b5796bbe1dbcc9e87958163c42307db
2ood <kyungmin.official0@gmail.com>
2022-01-26 11:04:08 +0900

    refactor(clone_momentum_nomad) : modulized steps into functions

--- clone_momentum_nomad/script.js
+++ clone_momentum_nomad/script.js
@@ -8,39 +8,30 @@ const USERNAME_KEY="name";
 function onFormSubmitted(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  const userInput = loginInput.value;
+  localStorage.setItem(USERNAME_KEY,userInput);
 
-  localStorage.setItem(USERNAME_KEY,loginInput.value);
-
-  prepareTitle();
-  show(title);
-  console.dir(title);
+  showTitle(userInput);
+  hide(loginForm);
 }
 
-function prepareTitle() {
-  const username = localStorage.getItem(USERNAME_KEY);
+function showTitle(username) {
   title.innerText = `Hello, ${username}. `;
+  show(title);
+}
+
+function showLoginForm() {
+  show(loginForm);
+  loginForm.addEventListener("submit",onFormSubmitted);
 }
 
 function checkOnLoad() {
   const savedUsername = localStorage.getItem(USERNAME_KEY);
-  if(savedUsername===null) {
-    show(loginForm);
-    loginForm.addEventListener("submit",onFormSubmitted);
-    hide(title);
-  }
-  else {
-    prepareTitle();
-    show(title);
-    hide(loginForm);
-  }
+  if(savedUsername===null) showLoginForm();
+  else showTitle(savedUsername);
 }
 
-function hide(target) {
-  target.classList.add(HIDDEN_CLASSNAME);
-}
-
-function show(target) {
-  target.classList.remove(HIDDEN_CLASSNAME);
-}
+function hide(target) {target.classList.add(HIDDEN_CLASSNAME);}
+function show(target) {target.classList.remove(HIDDEN_CLASSNAME);}
 
 checkOnLoad();
