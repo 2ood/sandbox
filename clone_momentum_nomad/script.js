5f04c7672a299d7a54fdeeb5f308e09cbc569636
2ood <kyungmin.official0@gmail.com>
2022-01-26 10:26:15 +0900

    add(clone_momentum_nomad) :added localStorage saving

--- clone_momentum_nomad/script.js
+++ clone_momentum_nomad/script.js
@@ -1,7 +1,46 @@
-const title = document.getElementById("title");
-function handleTitleClick() {
-  console.log("the title was clicked");
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const title = document.getElementById("greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY="name";
+
+function onFormSubmitted(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+
+  localStorage.setItem(USERNAME_KEY,loginInput.value);
+
+  prepareTitle();
+  show(title);
+  console.dir(title);
 }
 
+function prepareTitle() {
+  const username = localStorage.getItem(USERNAME_KEY);
+  title.innerText = `Hello, ${username}. `;
+}
 
-title.addEventListener("click",handleTitleClick);
+function checkOnLoad() {
+  const savedUsername = localStorage.getItem(USERNAME_KEY);
+  if(savedUsername===null) {
+    show(loginForm);
+    loginForm.addEventListener("submit",onFormSubmitted);
+    hide(title);
+  }
+  else {
+    prepareTitle();
+    show(title);
+    hide(loginForm);
+  }
+}
+
+function hide(target) {
+  target.classList.add(HIDDEN_CLASSNAME);
+}
+
+function show(target) {
+  target.classList.remove(HIDDEN_CLASSNAME);
+}
+
+checkOnLoad();
