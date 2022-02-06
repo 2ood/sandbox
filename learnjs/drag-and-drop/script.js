8159f08a39682b4e1ddf41c0373a6319cbe99cc2
2ood <tommychoi0128@gmail.com>
2022-02-06 10:12:16 +0000

    made drag-and-drop example :

---
+++ learnjs/drag-and-drop/script.js
@@ -0,0 +1,29 @@
+let draggable = document.getElementsByClassName("draggable");
+
+for(i=0;i<draggable.length;i++) {
+    draggable[i].draggable = "true";
+    draggable[i].addEventListener("dragstart",onDragStart(event));
+}
+
+function onDragStart(event) {
+    return ;
+}
+
+function onDragEnd(event) {
+    return ;
+}
+function onDragEnter(event) {
+    return ;
+}
+function onDragExit(event) {
+    return ;
+}
+function onDragLeave(event) {
+    return ;
+}
+function onDragOver(event) {
+    return ;
+}
+function onDrop(event) {
+    return ;
+}
